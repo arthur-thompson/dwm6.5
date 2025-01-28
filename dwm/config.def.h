f6876ec3febc94ffe58bffb3606e01d4787da4df
Arthur Thompson <102389665+arthur-thompson@users.noreply.github.com>
2025-01-29 05:52:13 +1300

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=12" };
-static const char dmenufont[]       = "terminus:size=12";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#989898";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#4d4d4d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
